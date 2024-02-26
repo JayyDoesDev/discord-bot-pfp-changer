987e4047e3128977336e58f42557d5b2021b6c40
Eric Pan <75194970+EricPanDev@users.noreply.github.com>
2024-02-25 17:40:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import requests
+import base64
+import mimetypes
 
-api = "https://discord.com/api/v10/users/@me"
-token = "bot token"
+def file_to_data_uri(file_path):
+    with open(file_path, 'rb') as file:
+        binary_data = file.read()
 
-def change_bot_pfp_to(data_uri):
-    response = requests.patch(url=api, headers={"Authorization": f"Bot {token}"}, json={"avatar": f"{data_uri}"})
-    print(response.json())
+    base64_data = base64.b64encode(binary_data).decode('utf-8')
 
-with open("data_uri.txt", "r") as file:
-    change_bot_pfp_to(file.read())
+    # Determine the MIME type from file extension
+    mime_type, _ = mimetypes.guess_type(file_path)
+
+    if mime_type is None:
+        mime_type = 'application/octet-stream'
+
+    data_uri = f'data:{mime_type};base64,{base64_data}'
+
+    return data_uri
+
+if __name__ == "__main__":
+    
+    endpoint = "https://discord.com/api/v10/users/@me"
+    token = f"Bot {input('Please copy and paste your bot token: ')}"
+    filename = input("Please provide the path to the image you would like the bot to have: ")
+
+    response = requests.patch(endpoint, headers={"Authorization": token}, json={"avatar": file_to_data_uri(filename)})
+
+    if response.status_code == 200:
+        print("Bots profile picture has been successfully changed!")
+    else:
+        print("Discord's API returned non 200 HTTP response (%s)" % response.status_code)
+        print(response.text)
